8eb6acee697aaee5621ce0d7a511e52d83eafbcd
Daniel Karanja <61313029+agiradaniel@users.noreply.github.com>
2023-08-17 11:47:25 +0300

    tailwind updates

--- src/app/harrypotterapp/character/[id]/page.tsx
+++ src/app/harrypotterapp/character/[id]/page.tsx
@@ -7,8 +7,12 @@ import Image from 'next/image';
 import axios from 'axios';
 import HarryOutline from '../../components/images/celebrity-potter.jpg';
 import { AiFillHome } from 'react-icons/ai';
-import './character.css'
+import '../../styles/character.css'
 import Banner from '../../components/inc/banner';
+import { Circles } from 'react-loader-spinner';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'; // Import the CSS;
+
 
 interface CharacterData {
   id: string;
@@ -16,6 +20,7 @@ interface CharacterData {
   species: string;
   house: string;
   eyeColour: string;
+  image: string;
   wand: {
     wood: string;
     core: string;
@@ -26,19 +31,26 @@ interface CharacterData {
 
 const Character: React.FC = () => {
   const [specificCharacter, setSpecificCharacter] = useState<CharacterData[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
  
   const { id } = useParams();
 
   useEffect(() => {
-    const retrieveSpecificData = async () => {
+    try{
+      const retrieveSpecificData = async () => {
       
-      if (id && typeof id === 'string') {
-        const response = await axios.get(`https://hp-api.onrender.com/api/character/${id}`);
-        setSpecificCharacter(response.data);
-      }
-    };
-
-    retrieveSpecificData();
+        if (id && typeof id === 'string') {
+          const response = await axios.get(`https://hp-api.onrender.com/api/character/${id}`);
+          setSpecificCharacter(response.data);
+          setIsLoading(false)
+        }
+      };
+  
+      retrieveSpecificData();
+    }catch(error){
+      toast.error("An error occurred while retrieving data.");
+    }
+    
   }, [id]);
 
   return (
@@ -46,26 +58,35 @@ const Character: React.FC = () => {
       
       <Banner/>
 
+      <ToastContainer position="top-right" autoClose={4000} />
 
-      <Link href="/harrypotterapp" passHref>
-        <AiFillHome style={{ margin: '10px 0 0 5px', width: '30px', height: '30px' }}/>
+      <Link className='homeIconButton' href="/harrypotterapp" passHref>
+        <AiFillHome className='mt-2.5 ml-1.5 w-8 h-8'/>
       </Link>
 
-      {specificCharacter.map((character) => {
+      {!isLoading ?
+        <div className='characterDisplay'>
+
+        {specificCharacter.map((character) => {
         return (
+          
           <div key={character.id} className='mainCard'>
             <div className='imageDiv'>
+              {character.image ?
+              <img src={character.image}/>:
               <Image src={HarryOutline} alt="Top Banner" className="h-auto"/>
+              }
+              
             </div>
 
-            <div className='characterList' style={{color: 'white', textAlign: 'center', margin: '10px auto', fontWeight: 'bold', listStyle:"none"}}>
-              <h3 style={{ textDecoration: 'underline' }}>Character</h3>
+            <div className='characterList text-white text-center my-2.5 mx-auto font-bold list-none'>
+              <h3 className="underline underline-offset-4">Character</h3>
               <li>{character.name}</li>
               <li>Species: {character.species}</li>
               <li>House: {character.house}</li>
               <li>Eye Color: {character.eyeColour}</li>
               <li>DOB: {character.dateOfBirth || 'none'}</li>
-              <h4 style={{ textDecoration: 'underline' }}>Wand</h4>
+              <h4 className="underline underline-offset-4">Wand</h4>
               <li>Wood: {character.wand?.wood || 'none'}</li>
               <li>Core: {character.wand?.core || 'none'}</li>
               <li>Length: {character.wand?.length || 'none'}</li>
@@ -73,7 +94,23 @@ const Character: React.FC = () => {
             </div>
           </div>
         );
-      })}
+        })}
+        </div>
+
+        :
+
+        <div className="loaderSection mx-auto my-44 text-center w-28 h-28">
+        <Circles
+          height="80"
+          width="80"
+          color="#4fa94d"
+          ariaLabel="circles-loading"
+          wrapperStyle={{}}
+          wrapperClass=""
+          visible={true}
+        />
+        </div>
+      }
     </>
   );
 };
--- src/app/harrypotterapp/page.tsx
+++ src/app/harrypotterapp/page.tsx
@@ -4,22 +4,42 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Link from 'next/link';
 import Banner from './components/inc/banner';
+import { Circles } from 'react-loader-spinner';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'; // Import the CSS;
+import './styles/mainpage.css'
+
+interface Character {
+  id: number;
+  name: string;
+  dateOfBirth?: string;
+  house?: string;
+  image: string;
+}
 
 const Home: React.FC = () => {
   const [character, setCharacter] = useState<any[]>([]);
   const [searchQuery, setSearchQuery] = useState<string>('');
   const [filteredCharacter, setFilteredCharacter] = useState<any[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
-    const retrieveData = async () => {
-      const response = await axios.get(`https://hp-api.onrender.com/api/characters`);
-      setCharacter(response.data);
-    };
-
-    retrieveData();
+    try{
+      const retrieveData = async () => {
+        const response = await axios.get(`https://hp-api.onrender.com/api/characters`);
+        setCharacter(response.data);
+        setIsLoading(false)
+      };
+  
+      retrieveData();
+    }catch(error){
+      toast.error("An error occurred while retrieving data.");
+    }
+    
   }, []);
 
   useEffect(() => {
+    try{
     const filterCharacter = () => {
       if (searchQuery === '') {
         setFilteredCharacter(character);
@@ -32,6 +52,9 @@ const Home: React.FC = () => {
       }
     };
     filterCharacter();
+    }catch(error){
+      toast.error("An error occurred, try again later");
+    }
   }, [searchQuery, character]);
 
   return (
@@ -39,20 +62,20 @@ const Home: React.FC = () => {
       
       <Banner/>
 
-      <div style={{ textAlign: 'center', margin: '20px auto' }}>
+      <ToastContainer position="top-right" autoClose={4000} />
+
+      {!isLoading ? (
+      <>
+
+      <div className='searchArea text-center mx-auto my-5'>
         <input type="text" placeholder="Search by name or house..." value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)}
-          style={{
-            width: '200px',
-            height: '50px',
-            borderRadius: '10px',
-            margin: 'auto',
-            textAlign: 'center',
-            objectFit: 'cover',
-          }}
+          className='w-52 h-14 rounded-lg mx-auto text-center object-cover'
         />
       </div>
 
-      <div style={{display: 'flex', flexDirection: 'row', overflowX: 'auto', flexWrap: 'wrap', justifyContent: 'center', width: '100%'}}>
+
+
+      <div className='characterDetailsSection flex flex-row overflow-x-auto flex-wrap justify-center w-full'>
         {filteredCharacter.map((char) => {
           return (
             <Link key={char.id} href={`/harrypotterapp/character/${char.id}`} passHref style={{
@@ -60,10 +83,13 @@ const Home: React.FC = () => {
               color: 'inherit',
             }}>
               
-                <div style={{width: '250px', height: '150px', margin: '20px', backgroundColor: '#D6B445', textAlign: 'center', borderRadius: '5px',color: 'white'}}>
-                  <div style={{backgroundColor: 'white', width: '99%', height: '50px', border: '1px solid #D6B445', borderRadius: '5px 5px 0 0', margin: 'auto', color: 'black',}}>
+                <div className='characterCard w-64 h-40 m-5 text-white text-center rounded-md bg-cover bg-no-repeat' style={{backgroundColor: '#D6B445', backgroundImage: `url(${char.image})`}}>
+                  <div className='bg-white w-99 h-14 border rounded-t-md m-auto text-black opacity-80' style={{border: '1px solid #D6B445'}}>
+   
                     <h4>Character</h4>
+                    
                   </div>
+                  
                   <p>{char.name}</p>
                   <p>Born: {char.dateOfBirth || 'Not Specified'}</p>
                 </div>
@@ -71,7 +97,23 @@ const Home: React.FC = () => {
             </Link>
           );
         })}
+      </div> 
+      </>
+      ):
+
+      
+      <div className="loaderSection mx-auto my-44 text-center w-28 h-28">
+        <Circles
+          height="80"
+          width="80"
+          color="#4fa94d"
+          ariaLabel="circles-loading"
+          wrapperStyle={{}}
+          wrapperClass=""
+          visible={true}
+        />
       </div>
+  }
     </>
   );
 };
